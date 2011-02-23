f5102677135e17d1553bb259953eb4a9a050d363
Henrik Holmberg <henrik.holmberg89@gmail.com>
2011-02-23 12:50:45 +0100

    fixed ending tags bug

--- code/get_perl.pl
+++ code/get_perl.pl
@@ -10,88 +10,96 @@ use warnings;
 use Google::Search;
 use HTML::Entities;
 
+sub get_perl {
 
-my $arg1 = shift;
-my $search = Google::Search->Web(q => "site:$perldoc.perl.org $arg1");
-my $result = $search->first;
-my $url = $result->uri . "\n";
+    my $arg1 = shift;
+    my $search = Google::Search->Web("q" => "site:perldoc.perl.org $arg1");
+    my $result = $search->first;
+    my $url = $result->uri . "\n";
 
-print $search->error->reason, "\n" if $search->error;
+    print $search->error->reason, "\n" if $search->error;
 
-my @splittedurl = split /\//, $url;
-my $filename = pop(@splittedurl);
-my $file;
+    my @splittedurl = split /\//, $url;
+    my $filename = pop(@splittedurl);
+    my $file;
 
-`wget $url`;
+    my $out = "<?xml version=\"1.0\">\n<sections>";
 
-open $file, $filename or die $!;
+    `wget -q $url`;
+
+    open $file, $filename or die $!;
 
 
-my $section = 0;
+    my $section = 0;
 
-while (<$file>) {
-  
-  if(/<a name="[A-Za-z\-]*"><\/a><h2>([A-Za-z\-]*)/){
-    if ($section) {
-      print "</section>\n";
+    while (<$file>) {
+
+	if (/<a name="[A-Za-z\-]*"><\/a><h2>([A-Za-z\-]*)/){
+	    if ($section) {
+		$out .= "</content></section>\n";
+	    }
+	    $section = 1;
+	    $out .= "<section>\n";
+
+	}
+	if (/<div/) {
+	    $section = 0;
+	    $out .= "</content></section>";
+	}
+	if ($section) {
+	    if (/<pre class="verbatim">/) {
+		#KOD?! nämen va gott! äta taggar, formatera och skriva ut
+		s/<pre class="verbatim">/code\n/;
+		s/<\/pre>/\n\/code\n/;
+		s/<\/li>/\n/g;
+		s/<[^>]*>//g;
+
+		#fixa tecken!
+		s/&lt;/</g;
+		s/&gt;/>/g;
+		s/&#39;/'/g;
+
+
+		#koda tecken
+		encode_entities( $_, "<>&");
+		s/^code/<code>/g;
+		s/\/code/<\/code>/g;
+		s/&quot;/"/g;
+		$out .= $_;
+
+	    }elsif(/<h2>/){
+
+		# lägga dit överskrifter
+
+		s/<[^>]*>//g;
+		s/\n//g;
+		$out .= "<title>$_</title>\n<content>"
+
+	    }else {
+		# äta taggar och skriva vanlig text, formatera fnuttar
+		s/<[^>]*>//g;
+		s/&quot;/"/g;
+		s/&#39;/'/g;
+		encode_entities( $_, "<>&");
+		$out .= "$_";
+
+	}
+
+	}
+
+
     }
-    $section = 1;
-    print "<section>\n";
-  
-  }
-  if (/<div/) {
-    $section = 0;
-  }
-  if ($section) {
-    if (/<pre class="verbatim">/) {
-      #KOD?! nämen va gott! äta taggar, formatera och skriva ut
-      s/<pre class="verbatim">/code\n/;
-      s/<\/pre>/\n\/code\n/;
-      s/<\/li>/\n/g;
-      s/<[^>]*>//g;
 
-      #fixa tecken!
-      s/&lt;/</g;
-      s/&gt;/>/g;
-      s/&#39;/'/g;
-      
 
-      #koda tecken
-      encode_entities( $_, "<>&");
-      s/^code/<code>/g;
-      s/\/code/<\/code>/g;
-      s/&quot;/"/g;
-      print;
-    
-    }elsif(/<h2>/){
+    $out .= "</sections>";
 
-      # lägga dit överskrifter
-     
-      s/<[^>]*>//g;
-      s/\n//g;
-      print "<title>$_</title>\n"
 
-    }else {
-      # äta taggar och skriva vanlig text, formatera fnuttar
-      s/<[^>]*>//g;
-      s/&quot;/"/g;
-      s/&#39;/'/g;
-      encode_entities( $_, "<>&");
-      print"$_";
+    close $file or die $!;
 
-    }
+    `rm $filename`;
 
-  }
+    return $out;
 
-  
 }
-
-
-
-
-
-close $file or die $!;
-
-`rm $file`
 
 1;
--- code/q4code.pl
+++ code/q4code.pl
@@ -4,18 +4,23 @@ use strict;
 use warnings;
 use HTML::Entities;
 require "extract.pl";
+require "get_perl.pl";
 
 my @keywords = ( "for", "loop" );
 my $ext = Extract->new("perl", \@keywords);
 
-open(DUMMY, "extract-data.xml") || die("Could not open extract-data.xml\n");
-my $data = "";
-while (<DUMMY>) {
-    $data .= $_;
-}
-close(DUMMY);
+
+
+#open(DUMMY, "extract-data.xml") || die("Could not open extract-data.xml\n");
+my $data = get_perl("for loop");
+#while (<DUMMY>) {
+#    $data .= $_;
+#}
+#close(DUMMY);
+
 $ext->set_data($data);
-print decode_entities($ext->extract_code());
+my $result = $ext->extract_text();
+print decode_entities($result->{"title"} . "\n" . $result->{"content"} . "\n");
 print "Probability: " . $ext->get_probability() . "\n";
 
 1;
