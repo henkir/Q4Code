5aa14a52a3978b5b89d2173218d318d267e526ee
malvaron <vestlund.andreas@gmail.com>
2011-02-24 09:24:30 +0100

    Added file question-extract to the staging area
    This file will contain the code for extracting information from questions. As of now it only contains Perl testing.

---
+++ code/question-extract.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my @questions = ("What does a for-loop look like in Perl?",
+				 "What is the syntax of Python?",
+				 "How does input/ouput work in Perl?");
+
+
+print "Hello World!\n";
+
