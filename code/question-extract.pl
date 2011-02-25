5d1c24e330e592331254577b77ffef92bda16c88
malvaron <vestlund.andreas@gmail.com>
2011-02-25 23:08:55 +0100

    Added code to extract keywords. Only for-loops now.

--- code/question-extract.pl
+++ code/question-extract.pl
@@ -4,45 +4,57 @@ use warnings;
 
 my @questions = ("What does a for-loop look like in Perl?",
 				 "What is the syntax of Python?",
-				 "How does input/ouput work in Perl?");
+				 "How does input/ouput work in Perl?",
+				 "What is a for?",
+				 "What a for loop!");
 
 
 sub extract_specified_language {
-	my $str = shift;
+	my $question = shift;
 	my $specified_language = "ALL";
-	
-	if ($str =~ /([pP]erl|[pP]ython)/) {
+
+	if (lc($question) =~ /(perl|python)/) {
 		$specified_language = $1;
 	}
 	
 	return $specified_language;
 }
 
-for (my $i = 0; $i < @questions; $i = $i + 1) {
-	my $language = extract_specified_language($questions[$i]);
-	print $language . "\n";
+print "--- PRINTING EXTRACTED LANGUAGES ---\n";
+for (@questions) {
+	print extract_specified_language($_), "\n";
 }
 
-print "\n";
+sub find_keywords {
+	my $question = shift;
+	my $re = shift;
+	
+	my @keywords = ();
+	my $match = "";
+	do {
+		if ($question =~ /($re)(.*)/) {
+			$match = $1;
+			push(@keywords, $match);
+			$question = $2;
+		} else {
+			$match = ""
+		}
+	} while ($match);	
+	
+	return @keywords;
+}
 
-my @matches = ();
-my $match = "";
-my $next_str = "input is great for moar input/output";
-do {
-	print "looping\n";
-	print "next_str: " . $next_str . "\n";
-	if ($next_str =~ /(input|output)(.*)/) {
-		$match = $1;
-		push(@matches, $match);
-		$next_str = $2;
-	} else {
-		$match = ""
+print "\n--- PRINTING EXTRACTED KEYWORDS ---\n";
+for (@questions) {
+	print "Question: ", $_, "\n";
+	my @keywords = find_keywords($_, "for[- ]loops?|a for ");
+	
+	if (@keywords) {
+		#look for example or desc?
 	}
-} while ($match);
-
-print "\n--- PRINTING MATCHES ---\n";
-for (my $i = 0; $i < @matches; $i = $i +1) {
-	print $matches[$i] . "\n";
+	
+	print "\n";
 }
 
 
+1;
