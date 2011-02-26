9c34a7e742d624b42ffda7a0c46b08f4aa256984
malvaron <vestlund.andreas@gmail.com>
2011-02-26 19:42:18 +0100

    Look at previous comment...

--- code/question_extract.pl
+++ code/question_extract.pl
@@ -1,60 +1,107 @@
 #!/usr/bin/env perl
+
+###############################################################################
+# question_extract.pl
+# Provides a class Question_extract for extracting information from a provided 
+# question. Extracts keywords and programming language as of now.
+#
+###############################################################################
+
+package Question_extract;
+
 use strict;
 use warnings;
 
-my @questions = ("What does a for-loop look like in Perl?",
-				 "What is the syntax of Python?",
-				 "How does input/ouput work in Perl?",
-				 "What is a for?",
-				 "What a for loop!");
+sub new {
+	my $class = shift;
+	my $self  = {};
+	$self->{EXAMPLE}		= undef;
+	$self->{DESCRIPTION}	= undef;
+	$self->{LANGUAGE}		= undef;
+	$self->{KEYWORDS}		= ();
+	$self->{QUESTION}		= undef;
+	bless($self, $class);
+	return $self;
+}
 
+sub get_example {
+	my $self = shift;
+	return $self->{EXAMPLE};
+}
 
-sub extract_specified_language {
-	my $question = shift;
-	my $specified_language = "ALL";
+sub get_description {
+	my $self = shift;
+	return $self->{DESCRIPTION};
+}
 
-	if (lc($question) =~ /(perl|python)/) {
-		$specified_language = $1;
+sub get_language {
+	my $self = shift;
+	return $self->{LANGUAGE};
+}
+
+sub get_keywords {
+	my $self = shift;
+	return @{ $self->{KEYWORDS} };
+}
+
+sub get_question {
+	my $self = shift;
+	return $self->{QUESTION};	
+}
+
+sub set_question {
+	my $self = shift;
+	$self->{QUESTION} = shift;
+}
+
+sub extract_information {
+	my $self = shift;
+	
+	$self->extract_language();
+	$self->extract_keywords();
+}
+
+sub extract_language {
+	my $self = shift;
+	$self->{LANGUAGE} = "NONE";
+	if ($self->{QUESTION} =~ /(perl|python)/) {
+		$self->{LANGUAGE} = $1;
+	}
+}
+
+sub extract_keywords {
+	my $self = shift;
+	my $question = $self->{QUESTION};
+	
+	# FOR LOOPS
+	if ($question =~ /for[- ]loops?|a for /) {
+		push( @{ $self->{KEYWORDS} }, "for", "loop");
 	}
 	
-	return $specified_language;
-}
-
-print "--- PRINTING EXTRACTED LANGUAGES ---\n";
-for (@questions) {
-	print extract_specified_language($_), "\n";
-}
-
-sub find_keywords {
-	my $question = shift;
-	my $re = shift;
+	# WHILE LOOPS
+	if ($question =~ /a? ?while[- ]loops?/) {
+			push( @{ $self->{KEYWORDS} }, "while", "loop");
+	}
 	
-	my @keywords = ();
-	my $match = "";
-	do {
-		if ($question =~ /($re)(.*)/) {
-			$match = $1;
-			push(@keywords, $match);
-			$question = $2;
-		} else {
-			$match = ""
+	# FILE INPUT
+	if ($question =~ /((read|input) from a? ?file|file input)/) {
+		push( @{ $self->{KEYWORDS} }, "read from", "file");
+	} else {
+		# INPUT
+		if ($question =~ /(input)/) {
+			push( @{ $self->{KEYWORDS} }, "input");
 		}
-	} while ($match);	
-	
-	return @keywords;
-}
-
-print "\n--- PRINTING EXTRACTED KEYWORDS ---\n";
-for (@questions) {
-	print "Question: ", $_, "\n";
-	my @keywords = find_keywords($_, "for[- ]loops?|a for ");
-	
-	if (@keywords) {
-		#look for example or desc?
 	}
 	
-	print "\n";
+	# FILE OUTPUT
+	if ($question =~/((write|output) to a? ?file|file output)/) {
+		push( @{ $self->{KEYWORDS} }, "write to", "file");
+	} else {
+		# OUTPUT
+		if ($question =~ /(output|print)/) {
+			push( @{ $self->{KEYWORDS} }, "output");
+		}
+	}
 }
-
 
 1;
